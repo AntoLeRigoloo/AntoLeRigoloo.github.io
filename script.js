1adb21e089ea5865e419479f95aa67ace9767b79
AntoLeRigoloo <92756820+AntoLeRigoloo@users.noreply.github.com>
2022-12-22 18:59:23 +0100

    NewVersion
    new model new light etc...

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 import { GLTFLoader } from './3Dmodel/GLTFLoader.js';
-import { CSS3DRenderer, CSS3DObject } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/renderers/CSS3DRenderer.js';
-
+import { CSS3DRenderer, CSS3DObject } from './3Dtext/CSS3DRenderer.js';
+import { EffectComposer } from './Neon/EffectComposer.js';
+import { RenderPass } from './Neon/RenderPass.js';
+import { UnrealBloomPass } from './Neon/UnrealBloomPass.js';
+import { OrbitControls } from './OrbitControls.js';
 
 var scene = new THREE.Scene();
 
-
-const distance = 20;
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 1500 );
-camera.position.set(0,25,distance);
-camera.lookAt(0,25,0);
+const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,0.1,1000);
+let hauteur = 15;
+camera.position.set(0,hauteur,-15);
+camera.lookAt(0,hauteur,10);
 
 
 var renderer = new THREE.WebGLRenderer({
@@ -17,256 +19,14 @@ var renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setClearColor(0x000000, 1);
+renderer.setClearColor(0x000D15, 1);
 document.body.appendChild( renderer.domElement );
 
-
-
-
-
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    height = 2*window.innerHeight;
-    container.style.height = height + "px";
-    camera.updateProjectionMatrix();
-    let text = document.getElementById("label2");
-    text.style.width = 0.7*window.innerWidth + "px";
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    labelRenderer.setSize( innerWidth, innerHeight );
-}
-window.addEventListener('resize', onWindowResize, false);
-
-
-
-
-
-
-
-let ProgressBar = document.getElementById('ProgressBar');
-let DisplayBar = document.getElementById('DisplayBar');
-let status = document.getElementById('statusBar');
-const manager = new THREE.LoadingManager();
-
-manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
-    window.addEventListener("scroll", updateCamera);
-    console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-    status.textContent = itemsLoaded + ' of ' + itemsTotal + ' files.';
-    window.scrollBy({top: 1,behavior: 'smooth'});
-};
-manager.onLoad = function ( ) {
-    console.log( 'Loading complete!');
-    DisplayBar.style.display = "none";
-};
-manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
-    console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-    ProgressBar.style.width = (itemsLoaded / itemsTotal * 100) + '%';
-    status.textContent = itemsLoaded + ' of ' + itemsTotal + ' files.';
-};
-manager.onError = function ( url ) {
-    console.log( 'There was an error loading ' + url );
-};
-
-var loader = new GLTFLoader(manager);
-var obj;
-loader.load("3Dmodel/Orbiter_Space_Shuttle_OV-103_Discovery-150k-4096.gltf", function(gltf){
-    obj = gltf.scene;
-    obj.rotation.x = -Math.PI/1.7;
-    obj.rotation.z = 1.1*Math.PI;
-    obj.position.set(0,10,0);
-    scene.add(gltf.scene);
-});
-
-
-
-
-var light = new THREE.HemisphereLight( 0xFFFFFF, 0x000000, 1 );
-scene.add( light );
-const pointLight = new THREE.PointLight(0xFFFFFF, 2, 10000);
-pointLight.position.set(0, 0, -500);
-scene.add(pointLight);
-
-
-
-
-
-
-
-var labelRenderer = new CSS3DRenderer();
-labelRenderer.setSize( innerWidth, innerHeight );
-labelRenderer.domElement.style.position = 'fixed';
-labelRenderer.domElement.style.top = '0px';
-document.body.appendChild( labelRenderer.domElement );
-
-var box1 = new THREE.Mesh(new THREE.BoxBufferGeometry(14, 1, 0.5), new THREE.MeshBasicMaterial({color: 0xFFFFFF , wireframe: false, transparent: true, opacity: 0}));
-scene.add(box1);
-box1.position.set(0, 25, 12);
-
-let d1;
-var pos1 = new THREE.Vector3(0, 0, 0.1);
-var normal1 = new THREE.Vector3(0, 0, 1);
-var cNormal1 = new THREE.Vector3();
-var cPos1 = new THREE.Vector3();
-var m41 = new THREE.Matrix4();
-
-var div1 = document.getElementById("Acceuil");
-var label1 = new CSS3DObject( div1 );
-label1.position.copy(pos1);
-label1.rotation.y = 0;
-label1.scale.set(0.025, 0.025, 1);
-box1.add( label1 );
-
-var box4 = new THREE.Mesh(new THREE.BoxBufferGeometry(14, 1, 0.5), new THREE.MeshBasicMaterial({color: 0xFFFFFF , wireframe: false, transparent: true, opacity: 0}));
-scene.add(box4);
-box4.position.set(12, 18.75, 0);
-
-let d4;
-var pos4 = new THREE.Vector3(0.1, 0, 0);
-var normal4 = new THREE.Vector3(1, 0, 0);
-var cNormal4 = new THREE.Vector3();
-var cPos4 = new THREE.Vector3();
-var m44 = new THREE.Matrix4();
-
-var div4 = document.getElementById("Events");
-var label4 = new CSS3DObject( div4 );
-label4.position.copy(pos4);
-label4.rotation.y = 0.5*Math.PI;
-label4.scale.set(0.025, 0.025, 1);
-box4.add( label4 );
-
-var box2 = new THREE.Mesh(new THREE.BoxBufferGeometry(14, 1, 0.5), new THREE.MeshBasicMaterial({color: 0xFFFFFF , wireframe: false, transparent: true, opacity: 0}));
-scene.add(box2);
-box2.position.set(0, 12.5, -12);
-
-let d2;
-var pos2 = new THREE.Vector3(0, 0, 0.1);
-var normal2 = new THREE.Vector3(0, 0, -1);
-var cNormal2 = new THREE.Vector3();
-var cPos2 = new THREE.Vector3();
-var m42 = new THREE.Matrix4();
-
-var div2 = document.getElementById("Equipe");
-var label2 = new CSS3DObject( div2 );
-label2.position.copy(pos2);
-label2.rotation.y = Math.PI;
-label2.scale.set(0.025, 0.025, 1);
-box2.add( label2 );
-
-var box5 = new THREE.Mesh(new THREE.BoxBufferGeometry(14, 1, 0.5), new THREE.MeshBasicMaterial({color: 0xFFFFFF , wireframe: false, transparent: true, opacity: 0}));
-scene.add(box5);
-box5.position.set(-12, 6.25, 0);
-
-let d5;
-var pos5 = new THREE.Vector3(-0.1, 0, 0);
-var normal5 = new THREE.Vector3(-1, 0, 0);
-var cNormal5 = new THREE.Vector3();
-var cPos5 = new THREE.Vector3();
-var m45 = new THREE.Matrix4();
-
-var div5 = document.getElementById("Sponsors");
-var label5 = new CSS3DObject( div5 );
-label5.position.copy(pos5);
-label5.rotation.y = -0.5*Math.PI;
-label5.scale.set(0.025, 0.025, 1);
-box5.add( label5 );
-
-var box3 = new THREE.Mesh(new THREE.BoxBufferGeometry(14, 1, 0.5), new THREE.MeshBasicMaterial({color: 0xFFFFFF , wireframe: false, transparent: true, opacity: 0}));
-scene.add(box3);
-box3.position.set(0, 0, 12);
-
-let d3;
-var pos3 = new THREE.Vector3(0, 0, 0.1);
-var normal3 = new THREE.Vector3(0, 0, 1);
-var cNormal3 = new THREE.Vector3();
-var cPos3 = new THREE.Vector3();
-var m43 = new THREE.Matrix4();
-
-var div3 = document.getElementById("Contacts");
-var label3 = new CSS3DObject( div3 );
-label3.position.copy(pos3);
-label3.rotation.y = 0;
-label3.scale.set(0.025, 0.025, 1);
-box3.add( label3 );
-
-
-
-
-
-
-
-const SunTextureLoader = new THREE.TextureLoader().load("BGtextures/sun.jpg");
-const mercuryTexture = new THREE.TextureLoader().load( './BGtextures/mercury.jpg');
-const venusTexture = new THREE.TextureLoader().load( './BGtextures/venus.jpg');
-const earthTexture = new THREE.TextureLoader().load( './BGtextures/earth.jpg');
-const marsTexture = new THREE.TextureLoader().load( './BGtextures/mars.jpg');
-const jupiterTexture = new THREE.TextureLoader().load( './BGtextures/jupiter.jpg');
-const saturnTexture = new THREE.TextureLoader().load( './BGtextures/saturn.jpg');
-const saturnRingTexture = new THREE.TextureLoader().load( './BGtextures/saturn ring.png');
-const uranusTexture = new THREE.TextureLoader().load( './BGtextures/uranus.jpg');
-const uranusRingTexture = new THREE.TextureLoader().load( './BGtextures/uranus ring.png');
-const neptuneTexture = new THREE.TextureLoader().load( './BGtextures/neptune.jpg');
-
-const sunGeo = new THREE.SphereGeometry(40, 150, 150);
-const sunMat = new THREE.MeshBasicMaterial({map: SunTextureLoader});
-const sun = new THREE.Mesh(sunGeo, sunMat);
-sun.position.set(0,0,-200);
-scene.add(sun);
-
-const earthGeo = new THREE.SphereGeometry(50, 150, 150);
-const earthMat = new THREE.MeshBasicMaterial({map: earthTexture});
-const earth = new THREE.Mesh(earthGeo, earthMat);
-earth.position.set(0,0,200);
-scene.add(earth);
-
-function createPlanete(size, texture, distance1, ring) {
-    const geo = new THREE.SphereGeometry(size, 30, 30);
-    const mat = new THREE.MeshStandardMaterial({
-        map: texture
-    });
-    const mesh = new THREE.Mesh(geo, mat);
-    const obj = new THREE.Object3D();
-    let ringMesh;
-    obj.position.set(0, 0, 0);
-    obj.add(mesh);
-    if(ring) {
-        const ringGeo = new THREE.RingGeometry(
-            ring.innerRadius,
-            ring.outerRadius,
-            32);
-        const ringMat = new THREE.MeshBasicMaterial({
-            map: ring.texture,
-            side: THREE.DoubleSide
-        });
-        ringMesh = new THREE.Mesh(ringGeo, ringMat);
-        obj.add(ringMesh);
-        ringMesh.position.x = distance1;
-        ringMesh.rotation.x = -0.5 * Math.PI;
-        ringMesh.rotation.y = 0.05 * Math.PI;
-    }
-    scene.add(obj);
-    mesh.position.x = distance1;
-    return {mesh, ringMesh , obj}
-}
-
-const mercury = createPlanete(10, mercuryTexture, 480);
-const venus = createPlanete(10, venusTexture, 520);
-const mars = createPlanete(10, marsTexture, 590);
-const jupiter = createPlanete(25, jupiterTexture, 680);
-const saturn = createPlanete(15, saturnTexture, 800, {innerRadius: 20,outerRadius: 30,texture: saturnRingTexture});
-const uranus = createPlanete(12, uranusTexture, 900, {innerRadius: 200,outerRadius: 200,texture: uranusRingTexture});
-const neptune = createPlanete(10, neptuneTexture, 1000);
-
-mercury.mesh.position.set(480,0,480);
-venus.mesh.position.set(-520,0,520);
-jupiter.mesh.position.set(680,0,-520);
-saturn.mesh.position.set(-800,0,-800);
-saturn.ringMesh.position.set(-800,0,-800);
-uranus.mesh.position.set(900,0,0);
-uranus.ringMesh.position.set(900,0,0);
-neptune.mesh.position.set(0,0,1000);
-mars.mesh.position.set(-590,0,0);
-
-
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.maxPolarAngle = Math.PI * 0.5;
+controls.minDistance = 10;
+controls.maxDistance = 50;
+controls.target.set(0, 10, 0);
 
 
 
@@ -277,130 +37,146 @@ function getRandomInt2(min, max) {
 }
 
 const starCount = 10000;
-const nothingAround = 40;
+const nothingAround = 50;
 let starBuffer = new THREE.BufferGeometry();
 let posstar = new Float32Array(starCount * 3);
 for (let i =0; i <(starCount*3); i+=3){
-    let x = getRandomInt2(-500,500);
-    let y = getRandomInt2(-500,500);
-    let z = getRandomInt2(-500,500);
+    let x = getRandomInt2(-100,100);
+    let y = getRandomInt2(-100,100);
+    let z = getRandomInt2(-100,100);
     while ((x < nothingAround)&&(x > -nothingAround)&&(y < nothingAround)&&(y > -nothingAround)&&(z < nothingAround)&&(z > -nothingAround)){
-        x = getRandomInt2(-500,500);
-        y = getRandomInt2(-500,500);
-        z = getRandomInt2(-500,500);
+        x = getRandomInt2(-100,100);
+        y = getRandomInt2(-100,100);
+        z = getRandomInt2(-100,100);
     }
     posstar[i] = x;
     posstar[i+1] = y;
     posstar[i+2] = z;  
 }
 starBuffer.setAttribute('position', new THREE.BufferAttribute(posstar, 3));
-let StarsTextureLoader = new THREE.TextureLoader().load('BGtextures/stars.png');
+let StarsTextureLoader = new THREE.TextureLoader().load('BGtextures/star.png');
 let starMaterial = new THREE.PointsMaterial({
-    color: 0xaaaaaa,
-    size: 0.7,
+    color: 0xcccccc,
+    size: 0.2,
     map : StarsTextureLoader,
-    transparent: true,
+    //alphaTest: 0.5,
 });
 let star = new THREE.Points(starBuffer, starMaterial);
 scene.add(star);
 
 
-let text = document.getElementById("test");
-let text2 = document.getElementById("test2");
-let text3 = document.getElementById("test3");
-let text4 = document.getElementById("test4");
-let text5 = document.getElementById("test5");
+
+class Spring extends THREE.Mesh{
+    constructor(radius, turns, segmentsPerTurn, height, growth, material){
+      let g = new THREE.CylinderGeometry(0.2, 0.2, 1, 16, segmentsPerTurn * turns).translate(0, 0.5, 0).rotateX(Math.PI * 0.5);
+      let initPos = g.attributes.position.clone();
+      super(g, material);
+      this.radius = radius;
+      this.turns = turns;
+      this.segmentsPerTurn = segmentsPerTurn;
+      this.height = height;
+      this.growth = growth;
+      this.update = () => {
+        let _n = new THREE.Vector3(0, 1, 0), _v3 = new THREE.Vector3(), _s = new THREE.Vector3();
+        let pos = g.attributes.position;
+        for(let i = 0; i < initPos.count; i++){
+          let ratio = initPos.getZ(i) * this.growth;
+          let angle = this.turns * Math.PI * 2 * ratio;
+          _v3.fromBufferAttribute(initPos, i).setZ(0);
+          _v3.applyAxisAngle(_n, angle + Math.PI * 0.5);
+          _v3.add(_s.setFromCylindricalCoords(this.radius, angle, this.height * ratio));
+          pos.setXYZ(i, ... _v3);
+        }
+        g.computeVertexNormals();
+        pos.needsUpdate = true;
+      }
+    }
+  }
+  
+
+var obj,mixer;
+var loader = new GLTFLoader();
+loader.load("3Dmodel/astroanim.gltf", function(gltf){
+    obj = gltf.scene;
+    obj.children[0].children[3].visible = false;
+    obj.rotation.y = Math.PI;
+    obj.scale.set(10,10,10);
+    obj.position.set(0,0,0);
+    scene.add(obj);
+    mixer = new THREE.AnimationMixer(obj);
+    const clips = gltf.animations;
+    const clip = THREE.AnimationClip.findByName(clips, 'ArmatureAction');
+    const action = mixer.clipAction(clip.optimize());
+    action.play();
+});
+
+
+
+
+
+const pointLight = new THREE.PointLight( 0x62BFFF, 0.52);
+scene.add( pointLight );
+pointLight.position.set( 0, 0, -10 );
+
+const pointLight2 = new THREE.PointLight( 0xFF48C5, 0.5);
+scene.add( pointLight2 );
+pointLight2.position.set( 10, 10, 10 );
+
+
+const renderScene = new RenderPass( scene, camera );
+const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+bloomPass.threshold = 0.5;
+bloomPass.strength = 2;
+bloomPass.radius = 1;
+bloomPass.brightness = 0.1;
+let composer = new EffectComposer( renderer );
+composer.addPass( renderScene );
+composer.addPass( bloomPass );
+
+
+                
+const material = new THREE.MeshBasicMaterial( {color: 0x62BFFF,} );
+
+let radius = 5;
+let turns = 2;
+let segmentsPerTurn = 100;
+let height = 15;
+let growth = 1;
+let spring = new Spring(radius, turns, segmentsPerTurn, height, growth, material);
+spring.update();
+scene.add(spring);
+spring.position.set(0,2,0);
+
+const geometry = new THREE.SphereGeometry( 1, 32, 16 );
+const material1 = new THREE.MeshBasicMaterial( { color: 0xFF48C5 } );
+const sphere = new THREE.Mesh( geometry, material1 );
+const sphere1= new THREE.Mesh( geometry, material1 );
+const sphere2 = new THREE.Mesh( geometry, material1 );
+const sphere3 = new THREE.Mesh( geometry, material1 );
+const sphere4 = new THREE.Mesh( geometry, material1 );
+
+
+scene.add( sphere );
+scene.add( sphere1 );
+scene.add( sphere2 );
+scene.add( sphere3 );
+scene.add( sphere4 );
+sphere.position.set(-14,10,6);
+sphere1.position.set(13,7,5);
+sphere2.position.set(7,12,15);
+sphere3.position.set(-8,15,-13);
+sphere4.position.set(14,7,-10);
 
 
 
 function animate() {
     requestAnimationFrame( animate );  
-    sun.rotation.y += 0.0005;
-    earth.rotation.y += 0.0005;
-    star.rotation.y += 0.00001;
-
-    mercury.mesh.rotateY(0.002);
-    venus.mesh.rotateY(0.001);
-    mars.mesh.rotateY(0.0002);
-    jupiter.mesh.rotateY(0.002);
-    saturn.mesh.rotateY(0.002);
-    uranus.mesh.rotateY(0.002);
-    neptune.mesh.rotateY(0.002);
-
-    //Around-sun-rotation
-    mercury.obj.rotateY(0.0003);
-    venus.obj.rotateY(0.00025);
-    mars.obj.rotateY(0.0001);
-    jupiter.obj.rotateY(0.00015);
-    saturn.obj.rotateY(0.0001);
-    uranus.obj.rotateY(0.0001);
-    neptune.obj.rotateY(0.0001);
-   
-
-    // cNormal.copy(normal).applyMatrix3(box.normalMatrix);
-    // cPos.copy(pos).applyMatrix4(m4.multiplyMatrices(camera.matrixWorldInverse, box.matrixWorld));
-    // d = cPos.negate().dot(cNormal);
-    // div.style.visibility = d < 0 ? "hidden" : "visible";
-
-    cNormal1.copy(normal1).applyMatrix3(box1.normalMatrix);
-    cPos1.copy(pos1).applyMatrix4(m41.multiplyMatrices(camera.matrixWorldInverse, box1.matrixWorld));
-    d1 = cPos1.negate().dot(cNormal1);
-    div1.style.visibility = d1 < 0 ? "hidden" : "visible";
-
-    cNormal4.copy(normal4).applyMatrix3(box4.normalMatrix);
-    cPos4.copy(pos4).applyMatrix4(m44.multiplyMatrices(camera.matrixWorldInverse, box4.matrixWorld));
-    d4 = cPos4.negate().dot(cNormal4);
-    div4.style.visibility = d4 < 0 ? "hidden" : "visible";
-
-    cNormal2.copy(normal2).applyMatrix3(box2.normalMatrix);
-    cPos2.copy(pos2).applyMatrix4(m42.multiplyMatrices(camera.matrixWorldInverse, box2.matrixWorld));
-    d2 = cPos2.negate().dot(cNormal2);
-    div2.style.visibility = d2 < 0 ? "hidden" : "visible";
-
-    cNormal5.copy(normal5).applyMatrix3(box5.normalMatrix);
-    cPos5.copy(pos5).applyMatrix4(m45.multiplyMatrices(camera.matrixWorldInverse, box5.matrixWorld));
-    d5 = cPos5.negate().dot(cNormal5);
-    div5.style.visibility = d5 < 0 ? "hidden" : "visible";
-
-    cNormal3.copy(normal3).applyMatrix3(box3.normalMatrix);
-    cPos3.copy(pos3).applyMatrix4(m43.multiplyMatrices(camera.matrixWorldInverse, box3.matrixWorld));
-    d3 = cPos3.negate().dot(cNormal3);
-    div3.style.visibility = d3 < 0 ? "hidden" : "visible";
-
-    text.style.opacity = (d1/8);
-    text2.style.opacity = (d2/8);
-    text3.style.opacity = (d3/8);
-    text4.style.opacity = (d4/8);
-    text5.style.opacity = (d5/8);
-
-
-
+    mixer.update(0.005);
+    controls.update();
     renderer.render( scene, camera );
-    labelRenderer.render( scene, camera );
-
+    composer.render();
 }
 
 animate();
 
 
-var container = document.createElement("div");
-container.id = "container";
-container.className = "container";
-let height = 2*window.innerHeight;
-container.style.height = height + "px";
-document.body.appendChild(container);
-
-
-
-
-
-function updateCamera(ev) {
-    //turn camera around the object
-    camera.position.y = 25 - (50*(window.scrollY/height));
-    let angle = 4*Math.PI*(window.scrollY/height);            
-    camera.position.x = distance * Math.sin(angle);
-    camera.position.z = distance * Math.cos(angle);
-    camera.lookAt(0,camera.position.y,0);
-
-
-}
